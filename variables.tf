d14d7c042ffea67486700d70504498c3e1dd033e
Zachary Rundle <zjrundle@gmail.com>
2022-09-13 13:32:33 -0500

    updated github actions

--- variables.tf
+++ variables.tf
@@ -10,25 +10,5 @@ variable "default_instance_type" {
 
 }
 
-variable "rules" {
-  type = list(object({
-    port        = number
-    proto       = string
-    cidr_blocks = list(string)
-  }))
-  default = [
-    {
-      port        = 80
-      proto       = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    },
-    {
-      port        = 443
-      proto       = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  ]
-}
-
 
  
